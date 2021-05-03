c9a412c5c0f5d6d82326c0d414f331af9a88c892
Apolonio Serafim <apolonio.serafim@hotmail.com>
2021-05-03 19:46:11 -0300

    create migrations to lend

---
+++ database/migrations/2021_05_03_184832_create_lends_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lends', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('returned')->default(false);
+            $table->date('return_forecast')->nullable();
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('lender_user_id');
+            $table->foreign('lender_user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lends');
+    }
+}
---
+++ database/migrations/2021_05_03_190515_create_lend_book_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLendBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lend_book', function (Blueprint $table) {
+            $table->id();
+            $table->string('comments')->nullable();
+            $table->timestamps();
+            $table->unsignedBigInteger('lend_id');
+            $table->unsignedBigInteger('book_id');
+            $table->foreign('lend_id')->references('id')->on('lends');
+            $table->foreign('book_id')->references('id')->on('books');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lend_book');
+    }
+}
